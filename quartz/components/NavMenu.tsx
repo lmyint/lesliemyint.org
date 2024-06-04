b540e4757cde33d672b9be1e5ffb72f0e0da2b9a
lmyint <lmyint@macalester.edu>
2024-06-04 11:30:05 -0500

    Fix paths in nav menu

--- quartz/components/NavMenu.tsx
+++ quartz/components/NavMenu.tsx
@@ -4,11 +4,11 @@ export default ((userOpts?: Options) => {
   function NavMenu(props: QuartzComponentProps) {
     return (
       <div class="navmenu">
-        <p>🌷 <a href="/writing/">Garden</a></p>
-        <p>💡 <a href="/teaching/">Teaching</a></p>
-        <p>🎤 <a href="/talks/">Talks</a></p>
+        <p>🌷 <a href="/writing">Garden</a></p>
+        <p>💡 <a href="/teaching">Teaching</a></p>
+        <p>🎤 <a href="/talks">Talks</a></p>
         <p>📜 <a href="/cv.pdf">CV</a></p>
-        <p>🛠️ <a href="/colophon/">Colophon</a></p>
+        <p>🛠️ <a href="/colophon">Colophon</a></p>
       </div>
     )
   }
