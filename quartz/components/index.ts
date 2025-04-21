23983c88f2640e0801d6e83e570dde42cc979ebc
lmyint <lmyint@macalester.edu>
2025-04-21 17:39:21 -0500

    Fix imports

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -20,6 +20,9 @@ import DesktopOnly from "./DesktopOnly"
 import MobileOnly from "./MobileOnly"
 import RecentNotes from "./RecentNotes"
 import Breadcrumbs from "./Breadcrumbs"
+import Comments from "./Comments"
+import Flex from "./Flex"
+import ConditionalRender from "./ConditionalRender"
 import NavMenu from "./NavMenu"
 
 export {
@@ -45,5 +48,8 @@ export {
   RecentNotes,
   NotFound,
   Breadcrumbs,
+  Comments,
+  Flex,
+  ConditionalRender,
   NavMenu
 }
